1914bdb038146fc5ac0cbc4f10886da470505a9f
Artem Potlov <potlov.artem@gmail.com>
2022-06-28 20:59:56 +0300

    feat: add api key

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9d3346e23bff4a21946284df12d2f287',
         });
     }
 }
