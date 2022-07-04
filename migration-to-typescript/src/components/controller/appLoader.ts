e3077103a10ca2a88f36e102a2fabcd0ee037b7b
Artem Potlov <potlov.artem@gmail.com>
2022-07-04 22:20:07 +0300

    fix: change api url

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '9d3346e23bff4a21946284df12d2f287',
         });
     }
