c6bb5a3171bd4e58decc2dbfed6e0e27d11f2672
Artem Potlov <potlov.artem@gmail.com>
2022-07-04 17:20:33 +0300

    refactor: migrate controller.js to controller.ts

--- migration-to-typescript/src/components/controller/controller.ts
+++ migration-to-typescript/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
 import AppLoader from './appLoader';
+import { CallBackFunc } from './loader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    public getSources(callback: CallBackFunc): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +11,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    public getNews(e: Event, callback: CallBackFunc): void {
+        let target: HTMLElement = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +32,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = (target as Node).parentNode as HTMLElement;
         }
     }
 }
