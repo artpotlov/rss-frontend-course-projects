dd3038baea4882d5c72f4175bd39ed2f77cf0c56
Artem Potlov <potlov.artem@gmail.com>
2022-07-04 17:18:20 +0300

    refactor: migrate app.js to app.ts

---
+++ migration-to-typescript/src/components/app/app.ts
@@ -0,0 +1,21 @@
+import AppController from '../controller/controller';
+import { CallBackFunc, IArticles, ISources } from '../controller/loader';
+import { AppView } from '../view/appView';
+
+class App {
+    private controller: AppController;
+    private view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        (document.querySelector('.sources') as Element).addEventListener('click', (e: Event) =>
+            this.controller.getNews(e, ((data: IArticles) => this.view.drawNews(data)) as CallBackFunc)
+        );
+        this.controller.getSources(((data: ISources) => this.view.drawSources(data)) as CallBackFunc);
+    }
+}
+
+export default App;
