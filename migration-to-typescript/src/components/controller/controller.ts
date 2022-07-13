c9d7f2ff3dfe163842ce159760022ea0d3ea55c2
Artem Potlov <potlov.artem@gmail.com>
2022-07-13 11:33:20 +0300

    refactor: update methods in AppController

--- migration-to-typescript/src/components/controller/controller.ts
+++ migration-to-typescript/src/components/controller/controller.ts
@@ -1,8 +1,8 @@
 import AppLoader from './appLoader';
-import { CallBackFunc } from './loader';
+import { CallBackFunc, IArticles, ISources } from './loader';
 
 class AppController extends AppLoader {
-    public getSources(callback: CallBackFunc): void {
+    public getSources(callback: CallBackFunc<ISources>): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -11,19 +11,21 @@ class AppController extends AppLoader {
         );
     }
 
-    public getNews(e: Event, callback: CallBackFunc): void {
-        const target = e.target as HTMLSelectElement;
-        const sourceId = target.value as string;
-        if (target.value !== '--') {
-            super.getResp(
-                {
-                    endpoint: 'everything',
-                    options: {
-                        sources: sourceId,
+    public getNews(e: Event, callback: CallBackFunc<IArticles>): void {
+        const target = e.target;
+        if (target) {
+            const sourceId = (<HTMLSelectElement>target).value;
+            if (sourceId !== '--') {
+                super.getResp(
+                    {
+                        endpoint: 'everything',
+                        options: {
+                            sources: sourceId,
+                        },
                     },
-                },
-                callback
-            );
+                    callback
+                );
+            }
         }
     }
 }
