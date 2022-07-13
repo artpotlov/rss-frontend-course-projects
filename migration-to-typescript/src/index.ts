53617bcd98cb527b9512435c7ebfbc6c7b2a90c3
Artem Potlov <potlov.artem@gmail.com>
2022-07-13 11:37:28 +0300

    fix: update app according to DI

--- migration-to-typescript/src/index.ts
+++ migration-to-typescript/src/index.ts
@@ -1,5 +1,14 @@
 import App from './components/app/app';
+import AppController from './components/controller/controller';
+import AppView from './components/view/appView';
+import News from './components/view/news/news';
+import Sources from './components/view/sources/sources';
 import './global.css';
 
-const app: App = new App();
+const news = new News();
+const sources = new Sources();
+const view = new AppView(news, sources);
+const appController = new AppController();
+
+const app = new App(appController, view);
 app.start();
