4391240935c9564e7f61d6d3eaf2b83d191b485f
Artem Potlov <potlov.artem@gmail.com>
2022-07-18 07:56:22 +0300

    fix: update base config import

--- online-store/webpack.prod.config.ts
+++ online-store/webpack.prod.config.ts
@@ -1,6 +1,6 @@
 import { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
-import baseConfig from './webpack.config';
+import { baseConfig } from './webpack.config';
 
 const prodConfig: Configuration = merge(baseConfig, {
   mode: 'production',
