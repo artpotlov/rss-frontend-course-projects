416ab45e10a6eb4bbd3ceaab21c9e16a2e78d801
Artem Potlov <potlov.artem@gmail.com>
2022-07-19 21:43:06 +0300

    feat: add Info panel view template

---
+++ online-store/src/components/view/info-panel/InfoPanelView.ts
@@ -0,0 +1,30 @@
+import './style.scss';
+
+export class InfoPanelView {
+  constructor (private readonly rootElement: HTMLElement) {}
+
+  draw() {
+    const template = `
+    <section class="info-panel">
+      <div>Men Shoes</div>
+        <select name="sortBy" id="sort-by" class="info-panel__select">
+          <option value="" disabled>Sort By</option>
+          <option value="name-asc">Name ascending</option>
+          <option value="name-desс">Name descending</option>
+          <option value="year-asc">Year ascending</option>
+          <option value="year-desс">Year descending</option>
+        </select>
+    </section>
+    `;
+
+    const element = document.createElement('div');
+    element.innerHTML = template.trim();
+    const result = element.firstChild;
+
+    if (!result) {
+      return ;
+    }
+
+    this.rootElement.appendChild(result);
+  }
+}
