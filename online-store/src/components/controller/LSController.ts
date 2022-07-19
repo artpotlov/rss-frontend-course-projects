b0d35a6627fc4ed70314b826111c273544c747e4
Artem Potlov <potlov.artem@gmail.com>
2022-07-19 21:59:16 +0300

    feat: add local storage controller

---
+++ online-store/src/components/controller/LSController.ts
@@ -0,0 +1,43 @@
+import { IFilter } from "../base/interface";
+
+export class LSController {
+  setFilters(conditionals: IFilter) {
+    if (!conditionals) {
+      return ;
+    }
+    localStorage.setItem('conditionals', JSON.stringify(conditionals));
+  }
+
+  getFilters() {
+    const conditionalsStr = localStorage.getItem('conditionals');
+
+    if(!conditionalsStr) {
+      return ;
+    }
+
+    const conditionals: IFilter = JSON.parse(conditionalsStr);
+    return conditionals;
+  }
+
+  getDataCart() {
+    const cartData = localStorage.getItem('cart');
+
+    if(!cartData) {
+      return [];
+    }
+
+    const productIDs: string[] = JSON.parse(cartData);
+    return productIDs;
+  }
+
+  setDataCart(products: string[]) {
+    if (!products) {
+      return ;
+    }
+    if (products.length < 21) {
+      localStorage.setItem('cart', JSON.stringify(products));
+      return 'ok';
+    }
+    return 'error';
+  }
+}
