c9979c5ceeb1981e396dd8feeb09b7c8af3c0264
Artem Potlov <potlov.artem@gmail.com>
2022-07-19 21:57:17 +0300

    feat: add popular controller

---
+++ online-store/src/components/controller/PopularController.ts
@@ -0,0 +1,55 @@
+import { IFilter } from "../base/interface";
+import { DataModel } from "../model/DataModel";
+import { AppView } from "../view/AppView";
+import { LSController } from "./LSController";
+
+export class PopularController {
+  constructor(
+    private readonly dataModel: DataModel,
+    private readonly appView: AppView,
+    private readonly lsController: LSController
+    ) {}
+
+  init() {
+    this.updateStates();
+    const checkbox = document.querySelector<HTMLElement>('.popular__list');
+    checkbox?.addEventListener('click', () => {
+      const checkbox = document.querySelector<HTMLInputElement>('input[type=checkbox][data-popular]');
+      const checkedCheckbox = checkbox?.checked;
+      this.updateCards(checkedCheckbox);
+    });
+  }
+
+  private updateCards(value = false) {
+    let filterConditionals: IFilter = { popular: false };
+    const conditionals = this.lsController.getFilters();
+
+    if (conditionals) {
+      filterConditionals = conditionals;
+    }
+
+    filterConditionals.popular = value;
+    this.lsController.setFilters(filterConditionals);
+
+    const filteringData = this.dataModel.updatefilters(filterConditionals);
+    this.appView.drawCards(filteringData, this.lsController.getDataCart());
+  }
+
+  updateStates(){
+    const checkbox = document.querySelector<HTMLInputElement>('input[type=checkbox][data-popular]');
+    if (!checkbox) {
+      return ;
+    }
+
+    const values = this.lsController.getFilters();
+    if (!values) {
+      return ;
+    }
+
+    if (values.popular) {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+  }
+}
