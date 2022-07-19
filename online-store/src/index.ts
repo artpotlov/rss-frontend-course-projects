b05609308e6e3e6c3f4fc533697b06d9afddb3e9
Artem Potlov <potlov.artem@gmail.com>
2022-07-19 21:30:04 +0300

    refactor: update error message

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -4,7 +4,7 @@ import { App } from './components/App';
 const appElement = document.querySelector<HTMLElement>('#app');
 
 if (!appElement) {
-  throw new Error();
+  throw new Error('App element not found on page');
 }
 
 const app = new App(appElement);
