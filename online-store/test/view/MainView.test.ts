92881b69f40ba3676bcb96717ec2c1dd16eca260
Artem Potlov <potlov.artem@gmail.com>
2022-07-24 18:24:12 +0300

    feat: add tests in MainView

--- online-store/test/view/MainView.test.ts
+++ online-store/test/view/MainView.test.ts
@@ -33,4 +33,53 @@ describe('Testing header view:', () => {
     mainView.draw();
     expect(app.innerHTML.trim().split(' ').join('')).toBe(template.trim().split(' ').join(''));
   });
+
+  test('check drawContent method with cards', () => {
+    mainView.draw();
+
+    const createCardElement = (text = '') => {
+      const cardElement = document.createElement('div');
+      cardElement.textContent = 'This is the card template' + text;
+      return cardElement;
+    }
+
+    mainView.drawContent([createCardElement(' 1'), createCardElement(' 2'), createCardElement(' 3')]);
+
+    const contentContainer = app.querySelector<HTMLElement>('.content');
+
+    expect(contentContainer?.childElementCount).toBe(3);
+    expect(contentContainer?.firstChild?.textContent).toBe('This is the card template 1');
+    expect(contentContainer?.lastChild?.textContent).toBe('This is the card template 3');
+  });
+
+  test('check drawContent method without cards', () => {
+    mainView.draw();
+    mainView.drawContent([]);
+
+    const errorMessageElement = '<h2 class="content__error">☹️ Sorry, no matches found</h2>';
+
+    const contentContainer = app.querySelector<HTMLElement>('.content');
+
+    expect(contentContainer?.innerHTML.trim().split(' ').join('')).toBe(errorMessageElement.trim().split(' ').join(''));
+  });
+
+  test('check drawFilter method with filter elements', () => {
+    mainView.draw();
+
+    const createFilterElement = (text = '') => {
+      const filterElement = document.createElement('div');
+      filterElement.textContent = 'This is the filter' + text;
+      return filterElement;
+    }
+
+    mainView.drawFilter(createFilterElement(' 1'));
+    mainView.drawFilter(createFilterElement(' 2'));
+    mainView.drawFilter(createFilterElement(' 3'));
+
+    const filterContainer = app.querySelector<HTMLElement>('.filter-panel__filters');
+
+    expect(filterContainer?.childElementCount).toBe(3);
+    expect(filterContainer?.firstChild?.textContent).toBe('This is the filter 1');
+    expect(filterContainer?.lastChild?.textContent).toBe('This is the filter 3');
+  });
 });
