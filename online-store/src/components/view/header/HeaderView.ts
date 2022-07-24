3f8787aa1b8297e59c84ca99b3f358b00d76e8b9
Artem Potlov <potlov.artem@gmail.com>
2022-07-24 17:35:27 +0300

    refactor: add indents and rename variable

--- online-store/src/components/view/header/HeaderView.ts
+++ online-store/src/components/view/header/HeaderView.ts
@@ -5,23 +5,23 @@ export class HeaderView {
 
   draw() {
     const template = `
-    <header class="header">
-      <div class="header__wrapper">
-        <div class="header__logo">Online Store</div>
-        <div class="header__input-wrapper">
-          <svg xmlns="http://www.w3.org/2000/svg" class="header__icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg>
-          <input type="search" name="searcher" id="searcher" placeholder="Search" class="header__input" autofocus autocompete="off">
+      <header class="header">
+        <div class="header__wrapper">
+          <div class="header__logo">Online Store</div>
+          <div class="header__input-wrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" class="header__icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <input type="search" name="searcher" id="searcher" placeholder="Search" class="header__input" autofocus autocompete="off">
+          </div>
+          <div class="header__shopping-cart">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+            </svg>
+            <span class="header__product-counter">20</span>
+          </div>
         </div>
-        <div class="header__shopping-cart">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-          </svg>
-          <span class="header__product-counter">20</span>
-        </div>
-      </div>
-    </header>
+      </header>
     `;
 
     const element = document.createElement('div');
@@ -36,12 +36,12 @@ export class HeaderView {
   }
 
   drawCounter(count = '0') {
-    const counter = document.querySelector<HTMLElement>('.header__product-counter');
+    const counterElement = this.rootElement.querySelector<HTMLElement>('.header__product-counter');
     
-    if (!counter) {
+    if (!counterElement) {
       return ;
     }
 
-    counter.textContent = count;
+    counterElement.textContent = count;
   }
 }
