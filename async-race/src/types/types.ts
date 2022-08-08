c9bef54da32c260aef59d7b7e3103929bc935ccf
Artem Potlov <potlov.artem@gmail.com>
2022-08-08 23:04:38 +0300

    feat: add types

---
+++ async-race/src/types/types.ts
@@ -0,0 +1,42 @@
+export enum RequestMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+export interface IBaseCar {
+  name: string;
+  color: string;
+}
+
+export interface ICar extends IBaseCar {
+  id: number;
+}
+
+export interface ICars {
+  totalCars: number;
+  cars: ICar[];
+}
+
+export interface IWinner {
+  id: number;
+  wins: number;
+  time: number;
+}
+
+export interface IWinnerCar extends IWinner, IBaseCar {}
+
+export interface IWinnerCars {
+  totalWinners: number;
+  winners: IWinnerCar[];
+}
+
+export interface IWinners {
+  totalWinners: number;
+  winners: IWinnerCar[];
+}
+
+export type Sort = 'id' | 'wins' | 'time';
+export type Order = 'ASC' | 'DESC';
