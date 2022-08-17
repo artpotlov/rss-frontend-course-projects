f930e78ecc6631fd86e13336f1bff5de8e87ca67
Artem Potlov <potlov.artem@gmail.com>
2022-08-17 13:51:14 +0300

    feat: add new methods and refactor other methods

--- async-race/src/utils/animation.ts
+++ async-race/src/utils/animation.ts
@@ -1,36 +1,76 @@
-import { IAnimationID } from '../types/types';
+import { IAnimation, IFastWinner } from '../types/types';
+import * as API from './api';
+import * as UI from '../ui/ui';
+import * as View from './view';
 
-const animationStore: IAnimationID = {};
+const animationStore: IAnimation = {};
+const fastWinner: IFastWinner = { id: -1, time: -1 };
 
-export function animation(
-  action: 'start' | 'stop' | 'reset',
+export function animationStart(
   carElement: HTMLElement,
   roadElement: HTMLElement,
+  id: number,
   duration = 1000,
-  id = 0,
 ) {
   let startTime = 0;
   const car = carElement;
   const carWidth = carElement.getBoundingClientRect().width;
   const distance = roadElement.getBoundingClientRect().width;
+  animationStore[id] = { animationId: 0, time: 0 };
 
-  if (action === 'start') {
-    animationStore[id] = requestAnimationFrame(function animationFunc(endTime) {
-      if (!startTime) startTime = endTime;
-      const progress = (endTime - startTime) / duration;
-      const translate = progress * (distance - carWidth);
-      car.style.transform = `translateX(${translate}px)`;
-      if (progress < 1) {
-        animationStore[id] = requestAnimationFrame(animationFunc);
+  animationStore[id].animationId = requestAnimationFrame(function animationFunc(endTime) {
+    if (!startTime) startTime = endTime;
+
+    const progress = (endTime - startTime) / duration;
+    const translate = progress * (distance - carWidth);
+    car.style.transform = `translateX(${translate}px)`;
+
+    if (progress < 1) {
+      animationStore[id].animationId = requestAnimationFrame(animationFunc);
+      return;
+    }
+
+    animationStore[id].time = (endTime - startTime) / 1000;
+  });
+}
+
+export function animationStop(id: number) {
+  cancelAnimationFrame(animationStore[id].animationId);
+}
+
+export function animationReset(carElement: HTMLElement, id: number) {
+  const car = carElement;
+  cancelAnimationFrame(animationStore[id].animationId);
+  car.style.transform = 'translateX(0)';
+}
+
+export function getFastWinner() {
+  return fastWinner;
+}
+
+export function resetFastWinner() {
+  fastWinner.id = -1;
+  fastWinner.time = -1;
+}
+
+export async function setWinner(id: number) {
+  if (fastWinner.id === -1) {
+    fastWinner.id = id;
+    fastWinner.time = Number(animationStore[id].time.toFixed(2));
+    const car = await API.getCar(id);
+
+    await API.updateWinner(fastWinner.id, fastWinner.time);
+
+    View.draw(UI.getWinnerAlert(car.name, fastWinner.time), document.body);
+
+    setTimeout(() => {
+      const alertElement = document.querySelector<HTMLElement>('.alert');
+
+      if (!alertElement) {
+        return;
       }
-    });
-  }
-  if (action === 'stop') {
-    cancelAnimationFrame(animationStore[id]);
-  }
 
-  if (action === 'reset') {
-    cancelAnimationFrame(animationStore[id]);
-    car.style.transform = 'translateX(0)';
+      View.removeElement(alertElement);
+    }, 3000);
   }
 }
